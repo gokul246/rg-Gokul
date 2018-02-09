5c98f1804537b6c437e0b85fa73972c83bbfbb56
gokul246 <35866872+gokul246@users.noreply.github.com>
2018-02-09 16:17:28 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+##include<conio.h>
+int main()
+{
+int a,b,c=0,x;
+printf("enter the numbers");
+scanf("%d",&a);
+x=a;
+for(i=0;a>10;i++);
+{
+b=a%10;
+a=a/10;
+c=c+pow(b,3);
+}
+c=c+pow(a,3);
+if(x==c)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
