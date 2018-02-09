2b0873fcc0d4d4852a37baf6819c6819c73ba8fd
gokul246 <35866872+gokul246@users.noreply.github.com>
2018-02-09 16:43:11 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int fact(int);
+int main()
+{
+  int a,f;
+  scanf("%d",&a);
+  f=fact(a);
+  printf("\nFactorial of %d is: %d",a,f);
+  return 0;
+}
+
+int fact(int n){
+   if(n==1)
+   	return 1;
+   else
+   	return(n*fact(n-1));
+ }
