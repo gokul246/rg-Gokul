2452e3f8abc8998b4a387978461c32e159fc2251
gokul246 <35866872+gokul246@users.noreply.github.com>
2018-02-12 13:15:35 +0530

    Create arithmaticprogression.c

---
+++ arithmaticprogression.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,a,d,c;
+printf("enter the number");
+scanf("%d%d%d",&n,&a,&d);
+for(i=1;i<5;i++)
+{
+c=a+(i*d);
+printf("%d",&c);
+}
+return 0;
+}
