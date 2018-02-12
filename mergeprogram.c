a51c47f191cfaa36161f07f9894f63bd730d8bc1
gokul246 <35866872+gokul246@users.noreply.github.com>
2018-02-12 14:25:08 +0530

    Update and rename array.c to mergeprogram.c

---
+++ mergeprogram.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int b,d[30],c;
+scanf("%d",&b);
+for(i=0;i<b;i++)
+{
+scanf("%d",&d(i));
+}
+for(i=0;i<(b-1);i++)
+if(d[i]>d[i+1])
+{
+c=d[i];
+printf("%d",c);
+}
+return 0;
+}
