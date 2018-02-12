ed144c778185daaacaaa4b43b72aa66daf627d9d
gokul246 <35866872+gokul246@users.noreply.github.com>
2018-02-12 13:44:06 +0530

    Create minimumarray.c

---
+++ minimumarray.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int n,a[30],c;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a(i));
+}
+for(i=0;i<(n-1);i++)
+if(a[i]<a[i+1])
+{
+c=a[i];
+}
+else
+{
+c=a[i+1];
+}
+printf("%d",c);
+return 0;
+}
