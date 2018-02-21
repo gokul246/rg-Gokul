50f6a3226243c71f94f7cc8cb7fc01256e19c9fa
gokul246 <35866872+gokul246@users.noreply.github.com>
2018-02-21 14:55:45 +0530

    Create max 10 numbers.c

---
+++ max 10 numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a[30],c;
+for(i=0;i<10;i++)
+{
+scanf("%d",&a(i));
+}
+for(i=0;i<9;i++)
+if(a[i]>a[i+1])
+{
+c=a[i];
+}
+else
+{
+c=a[i+1];
+}
+printf("%d",c);
+return 0;
+}
