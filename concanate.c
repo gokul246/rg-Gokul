ce63292098e531dc6d3b3db395b808b6281e679c
gokul246 <35866872+gokul246@users.noreply.github.com>
2018-02-26 13:44:37 +0530

    Update concanate.c

--- concanate.c
+++ concanate.c
@@ -1,16 +1,24 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
 #include<string.h>
-int main()
+int main(void) 
 {
-    int a,b,i,n,n1;
-    printf("enter the two numbers");
-    scanf("%d%d",&a,&b);
-    for(i=0;i<a;i++)
-    {
-    a=n;
-    n1=b;
-    b=n;
-    }
-    return 0;
+	char str1[100],str2[100];
+	int i,j;
+	printf("enter the string1:");
+	scanf("%s",str1);
+	printf("\n enter the string2:");
+	scanf("%s",str2);
+	for(i=0;str1[i]!='\0';i++)
+	{
+		
+	}
+	for(j=0;str2[j]!='\0';j++)
+	{
+		str1[i]=str2[j];
+		i++;
+	}
+	str1[i]='\0';
+	printf("\n%s",str1);
+	
+	return 0;
 }
