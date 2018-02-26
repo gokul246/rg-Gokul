10bd1923357887555a291879aaecc29852c3b79b
gokul246 <35866872+gokul246@users.noreply.github.com>
2018-02-26 13:42:07 +0530

    Create concanate.c

---
+++ concanate.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+    int a,b,i,n,n1;
+    printf("enter the two numbers");
+    scanf("%d%d",&a,&b);
+    for(i=0;i<a;i++)
+    {
+    a=n;
+    n1=b;
+    b=n;
+    }
+    return 0;
+}
