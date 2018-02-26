0071d1f910edd7a655296fee88863d31dd7381be
gokul246 <35866872+gokul246@users.noreply.github.com>
2018-02-26 13:54:44 +0530

    Create range.c

---
+++ range.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,i;
+clrscr();
+printf("enter the number:);
+scanf("%d%d",&a,&b);
+for(i=0;i<a;i++);
+{
+printf("\n,a);
+}
+for(i=0;i<b;i++);
+{
+printf("\n,b);
+}
+printf("highest number is...\n");
+getch();
+}
