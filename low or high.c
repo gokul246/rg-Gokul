7a040483596246ab63eb0529835676e059d87a69
gokul246 <35866872+gokul246@users.noreply.github.com>
2018-02-28 14:07:01 +0530

    Create low or high.c

---
+++ low or high.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a[10],n,i;
+clrscr();
+scanf("%d",&n);
+if(a(0)<a(i))
+{
+x=a(i);
+}
+else
+{
+x=a(0);
+}
+if(a(0)>a(i))
+{
+y=a(i);
+}
+else
+{
+y=a(0);
+}
+getch();
+}
