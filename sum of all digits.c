d559a402b96be3fc447ec57ba1b008a43f1121ab
gokul246 <35866872+gokul246@users.noreply.github.com>
2018-02-28 15:07:29 +0530

    Create sum of all digits.c

---
+++ sum of all digits.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+ {
+ int n1,n2,n3,h;
+ printf("enter the three number");
+ scanf("%d%d%d",&n1,&n2,&n3);
+ h=n1+n2+n3;
+ printf("the value is %d",h);
+ getch();
+ }
