53569f002016fb4428ae90495f244cd135417c92
gokul246 <35866872+gokul246@users.noreply.github.com>
2018-03-02 15:59:19 +0530

    Create odd or even.c

---
+++ odd or even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b,i,j;
+printf("enter the number");
+scanf("%d%d",&a,&b);
+j=a*b;
+if(j%==0)
+{
+printf("even number is %d");
+}
+else
+{
+printf("odd number is %d");
+}
+getch();
+}
