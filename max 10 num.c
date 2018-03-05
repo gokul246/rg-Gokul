86deaa95e16c401549445cb9521a9ee4be7dc995
gokul246 <35866872+gokul246@users.noreply.github.com>
2018-03-05 13:38:03 +0530

    Create max 10 num.c

---
+++ max 10 num.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int n,b[30],c;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&b(i));
+}
+for(i=0;i<(n-1);i++)
+if(b[i]>b[i+1])
+{
+c=b[i];
+}
+else
+{
+c=b[i+1];
+}
+printf("%d",c);
+return 0;
+}
