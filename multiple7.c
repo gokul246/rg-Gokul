c54dea2e03c65797f78d0e76b27d4e588bfda639
gokul246 <35866872+gokul246@users.noreply.github.com>
2018-03-07 14:40:47 +0530

    Create multiple7.c

---
+++ multiple7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,i;
+    printf("enter the number");
+    scanf("%d",&a);
+    if(a%7==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
