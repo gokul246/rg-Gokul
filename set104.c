9ead3e8e7d3478426705bc2e8da025dc43db4b91
gokul246 <35866872+gokul246@users.noreply.github.com>
2018-03-26 16:32:56 +0530

    Create set104.c

---
+++ set104.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,c;
+    printf("enter the two numbers");
+    scanf("%d%d",&a,&b);
+    {
+    c=pow(a,b);
+    printf("%d",c);
+    }
+    getch();
+}
