1c7de9fab44fa58c51a5a72eb4f3035edd15e48b
gokul246 <35866872+gokul246@users.noreply.github.com>
2018-03-26 16:52:09 +0530

    Create set107.c

---
+++ set107.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c,d;
+    printf("enter the three numbers");
+    scanf("%d%d%d",&a,&b,&c);
+    d=(a*b)/c;
+    printf("%d",d);
+    getch();
+}
